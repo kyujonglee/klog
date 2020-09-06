7380d81b56421d8092b37e6c336c88e64a9ea272
이규종 <kyujong93@naver.com>
2020-09-06 22:45:27 +0900

    [Edit] 오타수정

--- client/src/components/SocialLinks.tsx
+++ client/src/components/SocialLinks.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import { socialLinks } from "../constants/sns"
-import SocialLink from "./socialLink"
+import SocialLink from "./SocialLink"
 
 const List = styled.ul`
   display: flex;
