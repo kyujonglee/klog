e87b9edc2682d908f224234f5fe3373814790423
이규종 <kyujong93@naver.com>
2021-02-25 20:02:24 +0900

    [Edit] gatysby version

---
+++ src/pages/blog.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../template/Layout"
+
+function BlogPage() {
+  return <Layout>blog</Layout>
+}
+
+export default BlogPage
---
+++ src/pages/projects.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../template/Layout"
+
+function ProjectPage() {
+  return <Layout>projects</Layout>
+}
+
+export default ProjectPage
