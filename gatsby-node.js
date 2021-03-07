d429a47133a50591b997b7713fa12d1cecbd306b
이규종 <kyujong93@naver.com>
2021-03-07 20:31:40 +0900

    [Add] create personal project

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allStrapiProjects(sort: { order: DESC, fields: startDate }) {
+        nodes {
+          strapiId
+          slug
+        }
+      }
+    }
+  `)
+  result.data.allStrapiProjects.nodes.forEach(({ slug }) => {
+    createPage({
+      path: `projects/${slug}`,
+      component: path.resolve("./src/components/project/Project.tsx"),
+      context: {
+        slug,
+      },
+    })
+  })
+}
