76d771ca23835fd9b059e554144af3bb8a53966a
이규종 <kyujong93@naver.com>
2021-03-07 23:11:32 +0900

    [Add] ViewMore component

---
+++ src/components/common/ViewMore.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import styled, { CSSObject } from "styled-components"
+import { darken } from "polished"
+import { Link } from "gatsby"
+
+const Text = styled.span<{ color: Pick<TViewMoreProps, "color"> }>`
+  text-decoration: underline;
+  color: ${props => props.color ?? darken(0.3, "#d0bfff")};
+  font-weight: 600;
+  font-size: 1.125rem;
+`
+
+type TViewMoreProps = {
+  color?: CSSObject["color"]
+  text: string
+  url: string
+}
+
+function ViewMore({ text = "View More", color, url = "/" }: TViewMoreProps) {
+  return (
+    <Link to={url}>
+      <Text color={color}>&gt; {text}</Text>
+    </Link>
+  )
+}
+
+export default ViewMore
