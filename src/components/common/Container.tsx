e0615142a40a0c09b5972506277e97a6fd6f874b
이규종 <kyujong93@naver.com>
2021-03-08 23:01:54 +0900

    [Fix] isBrowser 일 때만 min-width

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,14 +1,11 @@
 import styled, { css } from "styled-components"
 import { isBrowser } from "react-device-detect"
+import { phoneMediaQuery } from "../../styles/responsive"
 
 const Container = styled.div`
   width: 90vw;
   max-width: 1170px;
   margin: 0 auto;
-  ${isBrowser &&
-  css`
-    min-width: 700px;
-  `}
 `
 
 export default Container
