bd66261c808779c73d741ce7b0b31860a42a7961
이규종 <kyujong93@naver.com>
2021-03-28 02:10:08 +0900

    [Add] blog markdown table css

--- src/components/common/MarkDownContainer.tsx
+++ src/components/common/MarkDownContainer.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { lighten } from "polished"
+import { lighten, darken } from "polished"
 
 const MarkDownContainer = styled.div`
   padding: 2rem;
@@ -56,6 +56,42 @@ const MarkDownContainer = styled.div`
     li {
       list-style: inside;
       margin: 0.25rem 0;
+    }
+    table {
+      background-color: white;
+      margin: 0.5rem 0;
+      border-radius: 0.5rem;
+      th,
+      td {
+        text-align: center;
+        vertical-align: middle;
+      }
+      th {
+        padding: 0.5rem;
+        font-weight: 500;
+        font-size: 1.125rem;
+        border-bottom: 1px solid ${props => props.theme.colors.lightGray2};
+        color: ${props => props.theme.colors.orange};
+        &:not(:first-child) {
+          border-left: 1px solid ${props => props.theme.colors.lightGray2};
+        }
+      }
+      td {
+        padding: 0.5rem;
+        border: 0.25px solid ${props => props.theme.colors.lightGray};
+        color: ${props => props.theme.colors.text};
+        border-bottom: 0;
+        &:first-child {
+          border-left: 0;
+          border-right: 1px solid ${props => props.theme.colors.lightGray2};
+          color: ${props => darken(0.15, props.theme.colors.lightOrange)};
+        }
+        &:last-child {
+          border-right: 0;
+        }
+      }
+      tr {
+      }
     }
   }
 `
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -10,6 +10,8 @@ const theme: DefaultTheme = {
     purple: "#6C4EF4",
     darkBlue: "#070711",
     gray: "#4C4B5C",
+    lightGray: "#e9ecef",
+    lightGray2: "#dee2e6",
     orange: "#FF6B01",
     lightOrange: "#FED5B3",
     red: "#E93683",
