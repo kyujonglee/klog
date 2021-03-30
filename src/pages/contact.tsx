01b5480531c1cb9ba5a87dedfa483ca4be68fb9e
이규종 <kyujong93@naver.com>
2021-03-30 22:37:27 +0900

    [Add] Contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,27 @@
 import React from "react"
-import Waiting from "../components/common/Waiting"
+import styled from "styled-components"
+import { FlexBox } from "../components/common"
+
 import Layout from "../template/Layout"
 
 function ContactPage() {
   return (
     <Layout>
-      <Waiting />
+      <Wrapper>📩 kyujong93@naver.com</Wrapper>
     </Layout>
   )
 }
 
 export default ContactPage
+
+const Wrapper = styled(FlexBox).attrs(() => ({
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+}))`
+  height: 85vh;
+  font-weight: 600;
+  font-size: 2rem;
+  background-color: white;
+  color: ${props => props.theme.colors.purple};
+`
