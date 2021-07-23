2264bd4a4b8c9515465185a587a432ee21c3e20f
이규종 <kyujong93@naver.com>
2021-07-23 16:20:55 +0900

    [Add] gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,54 @@ module.exports = {
         theme_color: `#F7F6F7`,
         display: `standalone`,
         icon: `src/images/blackbell.png`,
+        icons: [
+          {
+            src: "images/48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "images/72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "images/96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "images/144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "images/192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "images/256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "images/384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "images/512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`, `/about`, `/projects/*`, `/contact`],
       },
     },
   ],
