2b676049eca870a886f286e83635958638e19250
이규종 <kyujong93@naver.com>
2021-08-05 11:52:21 +0900

    [Add] Character component

---
+++ src/components/Character.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import Lottie from "react-lottie"
+import { isMobile } from "react-device-detect"
+import { lighten } from "polished"
+
+import animationData from "../lotties/cuty.json"
+import theme from "../styles/theme"
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData,
+}
+
+interface ICharacterProps {
+  color?: string
+}
+
+const Character = ({ color }: ICharacterProps) => {
+  return (
+    <Lottie
+      options={defaultOptions}
+      isMobile={isMobile}
+      style={{
+        background: color
+          ? color
+          : isMobile
+          ? // @ts-ignore
+            lighten(0.3, theme.colors.purple)
+          : "none",
+        borderRadius: "10px",
+      }}
+    />
+  )
+}
+
+export default Character
