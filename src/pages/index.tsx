db597e0d50c6cd547737df04d532f1666323a508
이규종 <kyujong93@naver.com>
2021-08-05 21:50:15 +0900

    Revert "[Add] react-lazyload"
    This revts commit 80f605ebc7b6449ae4af6c9e56b8af67f3b82637.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import LazyLoad from "react-lazyload"
 import Layout from "../template/Layout"
 import About from "../components/main/About"
 import Jobs from "../components/main/Jobs"
@@ -10,15 +9,9 @@ function Home() {
   return (
     <Layout>
       <SEO url={"/"} />
-      <LazyLoad once height={500}>
-        <About />
-      </LazyLoad>
-      <LazyLoad once height={500}>
-        <Jobs />
-      </LazyLoad>
-      <LazyLoad once height={500}>
-        <Projects />
-      </LazyLoad>
+      <About />
+      <Jobs />
+      <Projects />
     </Layout>
   )
 }
